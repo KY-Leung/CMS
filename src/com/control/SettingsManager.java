2ab33a0ebcf5f67b2d357eff18a42b0f08ca9c5e
mukhtar93 <muhammad.mukhtar.93@gmail.com>
2016-10-27 18:00:41 +0800

    Added retrieveuserlist for info dispatch
    -added user entity class
    -added the 4 retrieve methods in SettingsManager

--- src/com/control/SettingsManager.java
+++ src/com/control/SettingsManager.java
@@ -1,7 +1,10 @@
 package com.control;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import com.entity.Settings;
+import com.entity.User;
+
 
 
 public class SettingsManager {
@@ -72,5 +75,96 @@ public class SettingsManager {
 		}
 		
 		return s;
+	}
+	
+	public ArrayList<User> getHazeInfoUsers(){
+		ArrayList<User> list=new ArrayList<User>();
+		User user=null;
+		try{
+			DbController db=new DbController();
+			String query="select * from users where username in(select username from settings where hazeInfo=true);";
+			ResultSet rs=db.executeSql(query);
+			while(rs.next()){
+				user=new User();
+				user.setUsername(rs.getString("username"));
+				user.setName(rs.getString("name"));
+				user.setPhoneNumber(rs.getInt("phoneNumber"));
+				user.setType(rs.getInt("type"));
+				list.add(user);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public ArrayList<User> getFireInfoUsers(){
+		ArrayList<User> list=new ArrayList<User>();
+		User user=null;
+		try{
+			DbController db=new DbController();
+			String query="select * from users where username in(select username from settings where fireInfo=true);";
+			ResultSet rs=db.executeSql(query);
+			while(rs.next()){
+				user=new User();
+				user.setUsername(rs.getString("username"));
+				user.setName(rs.getString("name"));
+				user.setPhoneNumber(rs.getInt("phoneNumber"));
+				user.setType(rs.getInt("type"));
+				list.add(user);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public ArrayList<User> getMaskInfoUsers(){
+		ArrayList<User> list=new ArrayList<User>();
+		User user=null;
+		try{
+			DbController db=new DbController();
+			String query="select * from users where username in(select username from settings where maskInfo=true);";
+			ResultSet rs=db.executeSql(query);
+			while(rs.next()){
+				user=new User();
+				user.setUsername(rs.getString("username"));
+				user.setName(rs.getString("name"));
+				user.setPhoneNumber(rs.getInt("phoneNumber"));
+				user.setType(rs.getInt("type"));
+				list.add(user);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	public ArrayList<User> getBombshelterInfoUsers(){
+		ArrayList<User> list=new ArrayList<User>();
+		User user=null;
+		try{
+			DbController db=new DbController();
+			String query="select * from users where username in(select username from settings where bombshelterInfo=true);";
+			ResultSet rs=db.executeSql(query);
+			while(rs.next()){
+				user=new User();
+				user.setUsername(rs.getString("username"));
+				user.setName(rs.getString("name"));
+				user.setPhoneNumber(rs.getInt("phoneNumber"));
+				user.setType(rs.getInt("type"));
+				list.add(user);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public static void main(String[] args){
+		SettingsManager sm=new SettingsManager("asf");
+		System.out.println(sm.getBombshelterInfoUsers().size());
+		System.out.println(sm.getFireInfoUsers().size());
+		System.out.println(sm.getHazeInfoUsers().size());
+		System.out.println(sm.getMaskInfoUsers().size());
 	}
 }
---
+++ src/com/entity/User.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+public class User {
+	private String username;
+	private String name;
+	private int phoneNumber;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	private int type;
+	
+	
+}
